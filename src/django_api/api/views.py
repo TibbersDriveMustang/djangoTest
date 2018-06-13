0011f466841fc1b1a37e96327266bb983ffde042
Hongyi Guo <guohongyi1991@gmail.com>
2018-06-12 23:37:26 -0500

    create a api view

--- src/django_api/api/views.py
+++ src/django_api/api/views.py
@@ -3,4 +3,25 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
 # Create your views here.
+
+class HelloApiView(APIView):
+    """
+    Test API View.
+    """
+    def get(self, request, format=None):
+        """
+        Return a list of APIView features.
+        """
+        an_apiview = [
+            'Use HTTP method as function (get, post, patch, put, delete)'
+            'It is similiar to a traditional Django view',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs'
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
