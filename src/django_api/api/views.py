a2bddad20882119f76bbe02f9ef1fb0f9be4d585
Hongyi Guo <guohongyi1991@gmail.com>
2018-06-15 00:43:39 -0500

    added first viewset and router

--- src/django_api/api/views.py
+++ src/django_api/api/views.py
@@ -3,9 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from . import serializers
 
 # Create your views here.
 
@@ -13,6 +16,8 @@ class HelloApiView(APIView):
     """
     Test API View.
     """
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         """
         Return a list of APIView features.
@@ -25,3 +30,74 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """
+        Create a hello message with our name
+        :param request:
+        :return:
+        """
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk=None):
+        """
+            Handles updating an object
+        :param request:
+        :param pk:
+        :return:
+        """
+
+        return Response({'method': 'put'})
+
+
+    def patch(self, request, pk=None):
+        """
+            Patch request, only updates fields provided in the request
+        :param request:
+        :param pk:
+        :return:
+        """
+
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        """
+        Delete an object
+        :param request:
+        :param pk:
+        :return:
+        """
+
+        return Response({'method': 'delete'})
+
+class HelloViewSet(viewsets.ViewSet):
+    """
+        Test API ViewSet
+    """
+
+    def list(self, request):
+        """
+            Return a hello message
+        :param request:
+        :return:
+        """
+
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code.'
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+
+
+
