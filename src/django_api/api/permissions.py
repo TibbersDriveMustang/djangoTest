ed23efc1ceb94f0cadbfebfe45fe7a5b33e5b20c
Hongyi Guo <guohongyi1991@gmail.com>
2018-09-08 18:27:24 -0500

    Added virtualenv

--- src/django_api/api/permissions.py
+++ src/django_api/api/permissions.py
@@ -17,3 +17,22 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
+
+class PostOwnStatus(permissions.BasePermission):
+    """
+    Allow users to update their own status.
+    """
+    def has_object_permission(self, request, view, obj):
+        """
+        Check the user is trying to update their own status
+        :param request:
+        :param view:
+        :param obj:
+        :return:
+        """
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
--- src/django_api/api/views.py
+++ src/django_api/api/views.py
@@ -11,6 +11,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from . import serializers
 from . import models
@@ -190,3 +192,23 @@ class LoginViewSet(viewsets.ViewSet):
         :return:
         """
         return ObtainAuthToken().post(request)
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """
+    Handles creating, reading and updating profile feed items.
+    """
+
+    authentication_classes = (TokenAuthentication, )
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permissions_classes = (permissions.PostOwnStatus, IsAuthenticatedOrReadOnly)
+
+    def perform_create(self, serializer):
+        """
+        Sets the user profile to the logged in user.
+        :param serializer:
+        :return:
+        """
+
+        serializer.save(user_profile=self.request.user)
